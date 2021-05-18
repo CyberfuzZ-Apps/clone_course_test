d22f40aa45054635d45eff7dc66b01c7545f9135
CyberfuzZ <keepthelight@icloud.com>
2021-05-18 10:32:58 +0300

    1. Calculator

--- src/main/java/ru/parsentev/task_001/Calculator.java
+++ src/main/java/ru/parsentev/task_001/Calculator.java
@@ -11,28 +11,38 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 public class Calculator {
     private static final Logger log = getLogger(Calculator.class);
+    private double result;
 
     public double getResult() {
-        throw new UnsupportedOperationException();
+        return result;
+        //throw new UnsupportedOperationException();
     }
 
     public void add(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = first + second;
+        //throw new UnsupportedOperationException();
     }
 
     public void substract(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = first - second;
+        //throw new UnsupportedOperationException();
     }
 
     public void multiple(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = first * second;
+        //throw new UnsupportedOperationException();
     }
 
     public void div(double first, double second) {
-        throw new UnsupportedOperationException();
+        if (second == 0) {
+            throw new IllegalStateException();
+        }
+        result = first / second;
+        //throw new UnsupportedOperationException();
     }
 
     public void expand(double first, double second) {
-        throw new UnsupportedOperationException();
+        result = Math.pow(first, second);
+        //throw new UnsupportedOperationException();
     }
 }
