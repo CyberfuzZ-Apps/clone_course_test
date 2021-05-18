4d93d9bb71d839ba6636f35e727bf5583ffacab6
CyberfuzZ <keepthelight@icloud.com>
2021-05-18 10:47:49 +0300

    2. Point

--- src/main/java/ru/parsentev/task_002/Point.java
+++ src/main/java/ru/parsentev/task_002/Point.java
@@ -5,6 +5,10 @@ import org.slf4j.Logger;
 import static org.slf4j.LoggerFactory.getLogger;
 
 /**
+ * Реализовать класс точку, описывающую точку в системе координат x, y - Point(x, y).
+ * Объект точка должен иметь методы double Point#distanceTo(Point point) -
+ * метод должен вычислять расстояние между двумя точками.
+ *
  * Class Point defines a point in coordination system (x, y).
  * @author parsentev
  * @since 19.07.2016
@@ -21,6 +25,8 @@ public class Point {
     }
 
     public double distanceTo(final Point point) {
-        throw new UnsupportedOperationException();
+        int x = point.x - this.x;
+        int y = point.y - this.y;
+        return x + y;
     }
 }
